5d620c387d6970f793febc41e978dab036ce6b06
galej <jack.gale@tabcorp.com.au>
2024-02-18 12:58:58 +1100

    Building GUI

--- src/main/java/SwingGUI.java
+++ src/main/java/SwingGUI.java
@@ -1,12 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SwingGUI {
     public void GUI() {
         JFrame jfrm = new JFrame("ReportKisser");
         jfrm.setSize(500,500);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel jlbl = new JLabel("Kiss");
+
+//        Label
+        JLabel jlbl = new JLabel("REPORT KISSER");
+        jlbl.setFont(new Font("Lucida Console", Font.BOLD, 22));
+
+//        Drop Down
+
+//        Builder
         jfrm.add(jlbl);
+        jfrm.setLayout(null);
         jfrm.setVisible(true);
+    }
+    public void SwingFrame() {
+
+    }
+    public void ListFonts() {
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        for(String font : fonts){
+            System.out.println(font);
+        }
     }
 }
