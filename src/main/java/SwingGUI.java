e81fface1ad22d1d4c88883f793ccbafd6ce7794
galej <jack.gale@tabcorp.com.au>
2024-02-20 17:09:43 +1100

    Added Drop Down Menu

--- src/main/java/SwingGUI.java
+++ src/main/java/SwingGUI.java
@@ -1,23 +1,46 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class SwingGUI {
     public void GUI() {
         JFrame jfrm = new JFrame("ReportKisser");
         jfrm.setSize(500,500);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-//        Label
-        JLabel jlbl = new JLabel("REPORT KISSER");
-        jlbl.setFont(new Font("Lucida Console", Font.BOLD, 22));
-
-//        Drop Down
+        jfrm.getContentPane().setBackground(Color.white);
 
 //        Builder
-        jfrm.add(jlbl);
+        jfrm.add(TitleLabel());
+        jfrm.add(ProviderLabel());
+        jfrm.add(ProviderDropDown());
         jfrm.setLayout(null);
         jfrm.setVisible(true);
     }
+
+    public JLabel TitleLabel() {
+        JLabel jlbl = new JLabel("REPORT KISSER");
+        jlbl.setFont(new Font("Lucida Console", Font.BOLD, 25));
+        jlbl.setForeground(new Color(	88, 110, 117));
+        jlbl.setBounds(150,10,250,80);
+        return jlbl;
+    }
+
+    public JLabel ProviderLabel() {
+        JLabel jlbl = new JLabel("Select Data Provider");
+        jlbl.setBounds(155,75,200,25);
+        jlbl.setForeground(new Color(	88, 110, 117));
+        return jlbl;
+    }
+
+    public JComboBox<String> ProviderDropDown() {
+        String[] providers = {"HRV", "GRNSW", "GRV"};
+        Arrays.sort(providers);
+        final JComboBox<String> pDropDown = new JComboBox<>(providers);
+        pDropDown.setVisible(true);
+        pDropDown.setBounds(155,110,200,25);
+        return pDropDown;
+    }
+
     public void SwingFrame() {
 
     }
