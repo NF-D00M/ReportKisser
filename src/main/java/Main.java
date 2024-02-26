95fce1afe2cdfc3df84cbdec54a7675727727ce7
galej <jack.gale@tabcorp.com.au>
2024-02-26 14:55:57 +1100

    Adding File Provider Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import Providers.HRV;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Let's Kiss");
-        SwingGUI sg = new SwingGUI();
-            sg.GUI();
+
+//        SwingGUI sg = new SwingGUI();
+//            sg.GUI();
+
+        HRV hrv = new HRV();
+        hrv.ImportFile();
 
     }
 }
---
+++ src/main/java/Providers/HRV.java
@@ -0,0 +1,9 @@
+package Providers;
+
+public class HRV {
+    private String provider = "HRV";
+    public void ImportFile() {
+        System.out.println("Importing Data Provider: " + provider);
+
+    }
+}
