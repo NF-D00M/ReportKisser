a067ab425f2058bfabcb122b2e631ffb716e6651
galej <jack.gale@tabcorp.com.au>
2024-02-27 14:57:49 +1100

    Cleaned return methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,8 @@ public class Main {
 //            sg.GUI();
 
         HRV hrv = new HRV();
-        hrv.getTurnover();
+        hrv.setTurnover();
+
     }
 }
 
--- src/main/java/Providers/HRV.java
+++ src/main/java/Providers/HRV.java
@@ -5,25 +5,39 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
 
-
 public class HRV {
-    private String provider = "HRV";
+    private String Provider = "HRV";
+    public double FONet;
+    public double ToteNet;
 
-    public void getTurnover() throws IOException {
-        System.out.println("Retrieving Turnover for: " + provider);
+    public Workbook readWorkbook() throws IOException {
         String fileLocation = "src/main/resources/ProviderFiles/HRV/HRV_Data_May.xlsx";
         Workbook workbook = new XSSFWorkbook(fileLocation);
-        Sheet sheet = workbook.getSheetAt(0);
+        return workbook;
+    }
+
+    public void setTurnover() throws IOException {
+        System.out.println("Retrieving Turnover for: " + Provider);
+        readWorkbook();
+        Sheet sheet = readWorkbook().getSheetAt(0);
         for (Row row : sheet) {
+//            Get Row that Contains Total Turnover
             if (row.getCell(0).getStringCellValue().equals("Total Turnover")) {
                 System.out.println(row.getCell(0));
-
-                int FixedOddsNetTurnover = (int) row.getCell(6).getNumericCellValue();
-                int ToteNetTurnover = (int) row.getCell(10).getNumericCellValue();
-                System.out.println(FixedOddsNetTurnover);
-                System.out.println(ToteNetTurnover);
+                FONet = row.getCell(6).getNumericCellValue();
+                ToteNet = row.getCell(10).getNumericCellValue();
+                System.out.println("Setting Fixed Odds Net Turnover to: " + FONet);
+                System.out.println("Setting Tote Net Turnover to: " + ToteNet);
                 break;
             }
         }
+    }
+
+    public double getFixedOddsNetTurnover() {
+        return FONet;
+    }
+
+    public double getToteNetTurnover() {
+        return ToteNet;
     }
 }
